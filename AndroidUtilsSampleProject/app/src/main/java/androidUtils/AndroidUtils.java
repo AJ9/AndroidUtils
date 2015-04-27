9e27f6d73824aa9973f6c5d7b379b63e71ac726e
Adam Gask <ajgask@gmail.com>
2015-04-27 15:42:56 +0100

    Added resizing a list view

--- AndroidUtilsSampleProject/app/src/main/java/androidUtils/AndroidUtils.java
+++ AndroidUtilsSampleProject/app/src/main/java/androidUtils/AndroidUtils.java
@@ -2,6 +2,10 @@ package androidUtils;
 
 import android.content.Context;
 import android.os.Environment;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import java.io.File;
 
@@ -10,7 +14,7 @@ import java.io.File;
  */
 public class AndroidUtils {
 
-    
+
     public File getCacheFolder(Context context) {
         File cacheDir = null;
         if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
@@ -25,6 +29,45 @@ public class AndroidUtils {
         }
 
         return cacheDir;
+    }
+
+    /**
+     * Sets ListView height dynamically based on the height of the items.
+     *
+     * @param listView to be resized
+     * @return true if the listView is successfully resized, false otherwise
+     */
+    public static boolean setListViewHeightBasedOnItems(ListView listView) {
+
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter != null) {
+
+            int numberOfItems = listAdapter.getCount();
+
+            // Get total height of all items.
+            int totalItemsHeight = 0;
+            for (int itemPos = 0; itemPos < numberOfItems; itemPos++) {
+                View item = listAdapter.getView(itemPos, null, listView);
+                item.measure(0, 0);
+                totalItemsHeight += item.getMeasuredHeight();
+            }
+
+            // Get total height of all item dividers.
+            int totalDividersHeight = listView.getDividerHeight() *
+                    (numberOfItems - 1);
+
+            // Set list height.
+            ViewGroup.LayoutParams params = listView.getLayoutParams();
+            params.height = totalItemsHeight + totalDividersHeight;
+            listView.setLayoutParams(params);
+            listView.requestLayout();
+
+            return true;
+
+        } else {
+            return false;
+        }
+
     }
 
 }
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # AndroidUtils
+
+AssetManager mg = getResources().getAssets();
+
+try {
+String[] files = mg.list("");
+mg.open("test.pdf");
+
+} catch (IOException ex) {
+ex.printStackTrace();
+}
